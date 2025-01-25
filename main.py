90711dbe4376aa18b499203641e20af17b2c92c0
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2025-01-25 23:57:10 +0530

    added models of database and connected databse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from auth_routes import auth_router
+from order_routes import order_router
 
 app = FastAPI()
 
@@ -9,3 +11,6 @@ def read_root():
 @app.get("/hello/{name}")
 def say_hello(name: str):
     return {"message": f"Hello, {name}!"}
+
+app.include_router(auth_router)
+app.include_router(order_router)
